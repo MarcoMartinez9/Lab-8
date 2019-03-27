04573c40ce4c519465334f3d9cb221f19b19ab37
Marco Martinez <marco.mart1998@gmail.com>
2019-03-26 19:57:57 -0500

    Update Todo.java

---
+++ Todo.java
@@ -0,0 +1,25 @@
+
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-26-03
+OU ID_______________: 113424558
+OU EMAIL____________: marco.e.martinez-1@ou.edu
+
+Tasks:
+1. Complete enum classes
+2. Complete GamePiece class
+3. Complete BoardGame class
+
+
+Estimated Time (minutes):
+1. 30
+2. 35
+3. 40
+
+
+Actual Time (minutes):
+1. 40
+2. 40
+3. 40
+
+*/}
